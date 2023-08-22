61438c6747dd84499f3753b60cd56f20896b3e75
Rahul Rakshit <rahul.rakshit@posteo.net>
2023-08-22 21:17:55 +0200

    Implement happy path

--- n26/format_n26.go
+++ n26/format_n26.go
@@ -1,5 +1,30 @@
 package n26
 
-func FormatN26(csv []string) []string {
-	return csv
+import (
+	"encoding/csv"
+	"fmt"
+	"strings"
+)
+
+func FormatN26(inputLines []string) ([]string, error) {
+	outputLines := []string{"\"Date\",\"Vendor\",\"Reference\",\"Amount\""}
+
+	for index, inputLine := range inputLines {
+		if index == 0 {
+			continue
+		}
+
+		csvReader := csv.NewReader(strings.NewReader(inputLine))
+		row, _ := csvReader.Read()
+
+		date := row[0]
+		vendor := row[1]
+		reference := row[4]
+		amount := row[5]
+
+		outputLine := fmt.Sprintf("\"%s\",\"%s\",\"%s\",\"%s\"", date, vendor, reference, amount)
+		outputLines = append(outputLines, outputLine)
+	}
+
+	return outputLines, nil
 }
--- n26/format_n26_test.go
+++ n26/format_n26_test.go
@@ -9,15 +9,17 @@ import (
 )
 
 var _ = Describe("formatN26", func() {
-	It("says hello", func() {
-		sampleN26Csv, _ := os.ReadFile("./fixtures/sample_n26.csv")
-		sampleN26Lines := strings.Split(string(sampleN26Csv), "\n")
+	Context("In the happy path", func() {
+		It("it deletes a few columns from the in", func() {
+			sampleN26Csv, _ := os.ReadFile("./fixtures/sample_n26.csv")
+			sampleN26Lines := strings.Split(string(sampleN26Csv), "\n")
 
-    formatted := FormatN26(sampleN26Lines)
+			formatted, _ := FormatN26(sampleN26Lines)
 
-		expectedResultCsv, _ := os.ReadFile("./fixtures/expected_result.csv")
-		expectedResultLines := strings.Split(string(expectedResultCsv), "\n")
+			expectedResultCsv, _ := os.ReadFile("./fixtures/expected_result.csv")
+			expectedResultLines := strings.Split(string(expectedResultCsv), "\n")
 
-		Expect(formatted).To(Equal(expectedResultLines))
+			Expect(formatted).To(Equal(expectedResultLines))
+		})
 	})
 })
