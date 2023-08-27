506e82fe85a02d4d4c4c21a285e99b3c2ded4df2
Rahul Rakshit <rahul.rakshit@posteo.net>
2023-08-27 21:26:40 +0200

    Fix csv fs test if the test dir doesn't yet exist

--- csv/csv_test.go
+++ csv/csv_test.go
@@ -6,17 +6,25 @@ import (
 	"testing"
 )
 
+func createTestDir(dirPath string) {
+	_, err := os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		os.Mkdir(".test_temp", 0755)
+	}
+}
+
 func TestCsv(t *testing.T) {
-  data := [][]string{
-    {"1", "2", "3"},
-    {"a", "b", "c"},
-  }
-  filePath := ".test_temp/test.txt"
+	data := [][]string{
+		{"1", "2", "3"},
+		{"a", "b", "c"},
+	}
+	filePath := ".test_temp/test.csv"
+	createTestDir(".test_temp")
 
-  WriteCsv(data, filePath, "^")
-  fileContents, _ := ReadCsv(filePath, "^", 0)
+	WriteCsv(data, filePath, "^")
+	fileContents, _ := ReadCsv(filePath, "^", 0)
 
-  assert.Equal(t, data, fileContents)
+	assert.Equal(t, data, fileContents)
 }
 
 func TestCsv_CanSkipLines(t *testing.T) {
@@ -29,13 +37,14 @@ func TestCsv_CanSkipLines(t *testing.T) {
 		"a;b;c",
 	}
 	filePath := ".test_temp/complex.csv"
+	createTestDir(".test_temp")
 	file, _ := os.Create(filePath)
 	for _, line := range fileLines {
 		file.WriteString(line + "\n")
 	}
 	file.Close()
 
-  linesToSkip := 4
+	linesToSkip := 4
 	parsedData, _ := ReadCsv(filePath, ";", linesToSkip)
 
 	expectedData := [][]string{
