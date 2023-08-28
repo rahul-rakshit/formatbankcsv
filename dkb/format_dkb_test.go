3fbcfac85d2d1a33828f4757fb8de96cf4128dc3
Rahul Rakshit <rahul.rakshit@posteo.net>
2023-08-28 18:05:42 +0200

    Add dkb tests for unhappy path

--- dkb/format_dkb_test.go
+++ dkb/format_dkb_test.go
@@ -15,3 +15,17 @@ func TestFormatDkb_HappyPath(t *testing.T) {
 	expectedResultCsv, _ := csv.ReadCsv("./fixtures/expected_result.csv", ",", 0)
 	assert.Equal(t, expectedResultCsv, formatted)
 }
+
+func TestFormatN26_BadHeaderError(t *testing.T) {
+	badHeaderData, _ := csv.ReadCsv("./fixtures/bad_header_dkb.csv", ";", 4)
+
+	_, err := formatDkb(badHeaderData)
+
+	assert.Equal(t, "Unexpected header for dkb format", err.Error())
+}
+
+func TestFormatN26_OldFormatError(t *testing.T) {
+	_, err := csv.ReadCsv("./fixtures/old_dkb_format.csv", ";", 4)
+
+	assert.Regexp(t, "record on line 3: wrong number of fields", err.Error())
+}
